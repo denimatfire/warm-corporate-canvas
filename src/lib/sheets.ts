20f442f0de8a1c44eb89e77634402302c22199e3
DHRUBAJYOTI DAS <49041957+denimatfire@users.noreply.github.com>
2025-08-21 18:02:01 +0530

    Update sheets.ts

--- src/lib/sheets.ts
+++ src/lib/sheets.ts
@@ -1,4 +1,4 @@
-export const SHEETS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyX-lqkR5N-WVOtKTn81sq27HacvhEZjqRQhdoK--dR/dev"; // TODO: paste your deployed Apps Script Web App URL here
+export const SHEETS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwX-l-J9-4ZDcIGUEDFxbvWgmyMhvKPiMktPZjFXv7tKsVgB9Vrdlg-1OVy0gJm-95-/exec"; // TODO: paste your deployed Apps Script Web App URL here
 
 export type ContactFormData = {
 	name: string;
